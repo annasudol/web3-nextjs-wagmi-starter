99fe6c371e81f5493de558afb61e7697d9d42439
Anna <32278291+annasudol@users.noreply.github.com>
2025-01-12 18:31:17 +0100

    fix: update site URL in siteConfig for accuracy

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -4,7 +4,7 @@ export const siteConfig = {
   title: 'Web3 Starter',
   description: 'Start your Web3 project with this awesome template.',
   locale: 'en',
-  url: 'https://github.com/annasudol/',
+  url: 'https://github.com/annasudol/web3-nextjs-wagmi-starter',
   author: 'Anna Sudol',
   author_git: 'https://github.com/annasudol',
   txConfirmations: 1,
